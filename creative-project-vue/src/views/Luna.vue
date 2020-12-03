b34a49f0c8de9f120e54c854fa8d0e21281c9bf7
Joshua Reese <josh.reese.is@gmail.com>
2020-12-03 15:51:39 -0700

    Tinkering with the cat facts

--- creative-project-vue/src/views/Luna.vue
+++ creative-project-vue/src/views/Luna.vue
@@ -27,31 +27,33 @@
 
 <script>
 window.onload = function() {
+  var el = document.getElementById("get-cat-fact");
+  if (el) {
+    el.addEventListener("click", function(event) {
 
-  document.getElementById("get-cat-fact").addEventListener("click", function(event) {
+      // Prevent the default behavior
+      event.preventDefault();
 
-    // Prevent the default behavior
-    event.preventDefault();
+      // Fetch the current weather and format it
+      const url = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1";
+      fetch(url)
+        // Get the response and turn it into JSON
+        .then(function(response) {
+          return response.json();
 
-    // Fetch the current weather and format it
-    const url = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1";
-    fetch(url)
-      // Get the response and turn it into JSON
-      .then(function(response) {
-        return response.json();
+          // Format the results
+        }).then(function(json) {
 
-        // Format the results
-      }).then(function(json) {
+          console.log(json.text);
 
-        console.log(json.text);
+          let results = "<p class='title'>" + json.text + "</p>";
 
-        let results = "<p class='title'>" + json.text + "</p>";
-
-        document.getElementById("cat-fact-result").innerHTML = results;
+          document.getElementById("cat-fact-result").innerHTML = results;
 
 
-      });
-  });
+        });
+    });
+  }
 }
 </script>
 
